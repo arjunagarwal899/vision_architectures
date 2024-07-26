d2aaad53cabba431d04c21ee443c6be68c8b5fd7
Arjun Agarwal <arjun.agarwal@qure.ai>
2024-07-26 19:39:31 +0530

    activation checkpointing did not need to be a torch module

--- vision_architectures/activation_checkpointing.py
+++ vision_architectures/activation_checkpointing.py
@@ -4,18 +4,17 @@
 __all__ = ['ActivationCheckpointing']
 
 # %% ../nbs/10_activation_checkpointing.ipynb 2
-from torch import nn
 from torch.utils.checkpoint import checkpoint
 from typing import Callable
 
 # %% ../nbs/10_activation_checkpointing.ipynb 4
-class ActivationCheckpointing(nn.Module):
+class ActivationCheckpointing():
     def __init__(self, fn_checkpoint_level: int, training_checkpoint_level: int):
         super().__init__()
 
         self.perform_checkpointing = (fn_checkpoint_level <= training_checkpoint_level)
 
-    def forward(self, fn: Callable, *args):
+    def __call__(self, fn: Callable, *args):
         if self.perform_checkpointing:
             return checkpoint(lambda: fn(*args), use_reentrant=False)
         return fn(*args)
