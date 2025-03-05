3a254dce21867853f5c2fb22336ecca1ee7967da
Arjun Agarwal <arjun.agarwal@qure.ai>
2025-03-05 12:53:40 +0000

    added examples for activation checkpointing

--- vision_architectures/utils/activation_checkpointing.py
+++ vision_architectures/utils/activation_checkpointing.py
@@ -9,19 +9,20 @@ from typing import Callable
 from torch.utils.checkpoint import checkpoint
 
 # %% ../../nbs/utils/01_activation_checkpointing.ipynb 4
-class ActivationCheckpointing():
+class ActivationCheckpointing:
     """Activation checkpointing levels:
-        Level 0: No checkpointing
-        Level 1: Single layers are checkpointed e.g. linear / conv layers
-        Level 2: Small blocks are checkpointed e.g. residual blocks, attention blocks
-        Level 3: Medium-sized modules are checkpointed e.g. transformer layers, decoder blocks
-        Level 4: Large modules are checkpointed e.g. groups of transformer layers, decoder stages
-        Level 5: Very large modules are checkpointed e.g. entire encoders, decoders etc.
+    Level 0: No checkpointing
+    Level 1: Single layers are checkpointed e.g. linear / conv layers
+    Level 2: Small blocks are checkpointed e.g. residual blocks, attention blocks
+    Level 3: Medium-sized modules are checkpointed e.g. transformer layers, decoder blocks
+    Level 4: Large modules are checkpointed e.g. groups of transformer layers, decoder stages
+    Level 5: Very large modules are checkpointed e.g. entire encoders, decoders etc.
     """
+
     def __init__(self, fn_checkpoint_level: int, training_checkpoint_level: int):
         super().__init__()
 
-        self.perform_checkpointing = (fn_checkpoint_level <= training_checkpoint_level)
+        self.perform_checkpointing = fn_checkpoint_level <= training_checkpoint_level
 
     def __call__(self, fn: Callable, *args, **kwargs):
         if self.perform_checkpointing:
