corrected source link calculation

vision_architectures/utils/activation_checkpointing.py
@@ -32,26 +32,25 @@ class ActivationCheckpointing:
             training_checkpoint_level: Checkpointing level at which the model is being trained
 
         Example:
+            .. code-block:: python
 
-        .. code-block:: python
+                class MyModel(nn.Module):
+                    def __init__(self, training_checkpointing_level: int = 0):
+                        super().__init__()
+                        my_network = nn.Sequential(
+                            nn.Linear(784, 256),
+                            nn.ReLU(),
+                            nn.Linear(256, 10)
+                        )
 
-            class MyModel(nn.Module):
-                def __init__(self, training_checkpointing_level: int = 0):
-                    super().__init__()
-                    my_network = nn.Sequential(
-                        nn.Linear(784, 256),
-                        nn.ReLU(),
-                        nn.Linear(256, 10)
-                    )
+                        self.activation_checkpointing_level2 = ActivationCheckpointing(2, training_checkpointing_level)
 
-                    self.activation_checkpointing_level2 = ActivationCheckpointing(2, training_checkpointing_level)
+                    def forward(self, x):
+                        y = self.activation_checkpointing_level2(self.my_network, x)
+                        return y
 
-                def forward(self, x):
-                    y = self.activation_checkpointing_level2(self.my_network, x)
-                    return y
-
-        In this example, a ``training_checkpointing_level`` of greater than or equal to 2 will checkpoint ``my_network``
-        during training. If it's less than 2, the network will not be checkpointed.
+            In this example, a ``training_checkpointing_level`` of greater than or equal to 2 will checkpoint ``my_network``
+            during training. If it's less than 2, the network will not be checkpointed.
         """
         super().__init__()
 