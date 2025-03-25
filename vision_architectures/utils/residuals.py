8f99a24670babe9f0bcec5c7b347f83375c78e0f
Arjun Agarwal <arjunagarwal899@gmail.com>
2025-03-25 13:14:20 +0000

    added docs to residuals

--- vision_architectures/utils/residuals.py
+++ vision_architectures/utils/residuals.py
@@ -17,6 +17,15 @@ class Residual(nn.Module):
     """
 
     def forward(self, old_value: torch.Tensor, new_value: torch.Tensor):
+        """Simply adds the new value to the old value.
+
+        Args:
+            old_value: Old value to be added to.
+            new_value: New value to be added.
+
+        Returns:
+            Value after adding the new value to the old value.
+        """
         return old_value + new_value
 
 # %% ../../nbs/utils/05_residuals.ipynb 5
@@ -24,12 +33,33 @@ class StochasticDepthResidual(Residual):
     """This class can be wrapped around a list of modules to randomly drop some of them during training."""
 
     def __init__(self, survival_prob: float = 1.0, dropout_type: Literal["layer", "neuron"] = "layer"):
+        """Initializes the StochasticDepthResidual module.
+
+        Use ``dropout_type="layer"`` for most occasions as that implements true stochastic depth dropout as per SOTA
+        papers. Use `dropout_type="neuron"` only if you want to drop individual neurons.
+
+        Args:
+            survival_prob: Prbability that every layer / neuron will survive the residual connection. Defaults to 1.0.
+            dropout_type: Defaults to "layer".
+        """
+
         super().__init__()
         assert 1.0 >= survival_prob > 0.0, "Survival probability must be between (0, 1]"
         self.survival_prob = survival_prob
         self.dropout_type = dropout_type
 
     def forward(self, old_value: torch.Tensor, new_value: torch.Tensor):
+        """Drops the new value with a certain probability and scales the remaining value before adding it to the old
+        value. See :py:class:`Residual` for more details.
+
+        Args:
+            old_value: Old value to be added to.
+            new_value: New value to be added.
+
+        Returns:
+            Value after performing stochastic depth and adding the new value to the old value.
+        """
+
         if not self.training:
             return super().forward(old_value, new_value)
 
